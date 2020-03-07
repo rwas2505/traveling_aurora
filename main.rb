460f505163b7d5ec909c5012c8390b83aef1ab4b
Ryan <ryanwaszak@gmail.com>
2020-03-06 18:59:30 -0600

    add install_id variable and interpolation to main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require 'http'
 require 'json'
 # require './converter_data_hash.rb' #uncomment to call local hard code when off vpn
-
-response = HTTP.get("https://aurora-ordering.tesla.com/api/v1/installations/913758/geometry")
+# install_id = 1003287 #works
+# install_id = 1227028 #works
+install_id = 1470852
+response = HTTP.get("https://aurora-ordering.tesla.com/api/v1/installations/#{install_id}/geometry")
 
 #converter_data returns the value from the :site key needed for .json file
 converter_data = response.parse[0]["site"]
@@ -14,7 +16,7 @@ converter_data = JSON.parse converter_data.gsub('=>', ':')
 final = converter_data.to_json 
 # final = @converter_data.to_json #uncomment to call local hard code when off vpn from converter_data_hash.rb
 
-file = File.open("C:/Users/rwaszak/Desktop/Dev/aurora_grab/aurora_data.json", "w"){
+file = File.open("C:/Users/rwaszak/Desktop/Dev/aurora_grab/aurora_install_id_#{install_id}.json", "w"){
   |file| file.puts final} 
 
 
