29090543445b0911efb1dfa3f24d71ab733f1058
Ryan <ryanwaszak@gmail.com>
2020-03-06 23:21:16 -0600

    add various save methods for reference

--- main.rb
+++ main.rb
@@ -4,8 +4,10 @@ require 'json'
 
 #input coming from user form in excel wkbk via shell function
 vba_input = ARGV
-job_number = vba_input[0] 
+job_number = vba_input[0]
+first_three = job_number[0..2]
 install_id = vba_input[1]
+file_path = "//triton/jobs/#{first_three}/#{job_number}/Audit/sample.txt"
 
 #get aurora api data based on user input install id
 response = HTTP.get("https://aurora-ordering.tesla.com/api/v1/installations/#{install_id}/geometry")
@@ -20,7 +22,37 @@ converter_data = JSON.parse converter_data.gsub('=>', ':')
 final = converter_data.to_json 
 
 #save locally 
-file = File.open("C:/Users/rwaszak/Desktop/Dev/aurora_grab/generated_jsons/aurora_install_id_#{install_id}.json", "w"){
-  |file| file.puts final} 
+# file = File.open("C:/Users/rwaszak/Desktop/Dev/aurora_grab/generated_jsons/aurora_install_id_#{install_id}.json", "w"){
+# |file| file.puts final} 
+
+#save locally with variable works
+# sample_text = "jb: #{job_number}, first three: #{first_three}, install id: #{install_id}"
+# local_path = "C:/Users/rwaszak/Desktop/Dev/aurora_grab/generated_jsons/sample.txt"
+# file = File.open(local_path, "w"){
+# |file| file.puts sample_text} 
+
+
+#save to triton with no vars works!
+# sample_text = "test2"
+# file = File.open("//triton/jobs/#{first_three}/#{job_number}/Audit/sample.txt", "w"){
+# |file| file.puts sample_text} 
+
+
+#save to triton with  vars works!
+# sample_text = "test2"
+# file_path = "//triton/jobs/#{first_three}/#{job_number}/Audit/sample.txt"
+file = File.open(file_path, "w"){
+|file| file.puts sample_text} 
+
+
+
+
+
+
+
+
+
+
+
 
 
