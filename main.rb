26388152b169ec6a0360c1ac6d2cfcca15acf4ff
Ryan <ryanwaszak@gmail.com>
2020-03-06 22:24:55 -0600

    add working version of excel wkbk for user input

--- main.rb
+++ main.rb
@@ -1,9 +1,13 @@
 require 'http'
 require 'json'
 # require './converter_data_hash.rb' #uncomment to call local hard code when off vpn
-# install_id = 1003287 #works
-# install_id = 1227028 #works
-install_id = 1470852
+
+#input coming from user form in excel wkbk via shell function
+vba_input = ARGV
+job_number = vba_input[0] 
+install_id = vba_input[1]
+
+#get aurora api data based on user input install id
 response = HTTP.get("https://aurora-ordering.tesla.com/api/v1/installations/#{install_id}/geometry")
 
 #converter_data returns the value from the :site key needed for .json file
@@ -14,14 +18,9 @@ converter_data = converter_data.to_s
 converter_data = converter_data.gsub("nil", "[]")
 converter_data = JSON.parse converter_data.gsub('=>', ':')
 final = converter_data.to_json 
-# final = @converter_data.to_json #uncomment to call local hard code when off vpn from converter_data_hash.rb
 
-file = File.open("C:/Users/rwaszak/Desktop/Dev/aurora_grab/aurora_install_id_#{install_id}.json", "w"){
+#save locally 
+file = File.open("C:/Users/rwaszak/Desktop/Dev/aurora_grab/generated_jsons/aurora_install_id_#{install_id}.json", "w"){
   |file| file.puts final} 
 
 
-
-#https://stackoverflow.com/questions/32388756/replacing-all-null-occurence-in-ruby-string
-#https://stackoverflow.com/questions/1895295/how-can-i-replace-every-instance-of-a-pattern-in-ruby
-#https://stackoverflow.com/questions/1667630/how-do-i-convert-a-string-object-into-a-hash-object
-#https://stackoverflow.com/questions/24543826/removing-backslash-escape-character-from-a-string#
